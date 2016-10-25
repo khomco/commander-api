c30f912c1af76410b54f76102d1b0c327edfab02
Keith Homco <keith.homco@centurylink.com>
2016-10-24 19:36:57 -0500

    modified response to return the incoming id of command

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,22 @@ var router = express.Router();
 var kafka = require('../lib/kafka');
 var kafka_producer = kafka.getInstance();
 
-router.post('/commands', function (req, res) {
+router.post('/commands', (req, res) => {
     console.log("Received command: " + JSON.stringify(req.body))
     var command = registry.lookupCommand(req);
     if (command === undefined) {
         res.status(400).json({"message": "command was not accepted"})
     } else {
-        command.validate(function (result) {
+        command.validate((result) => {
             if (result.errors.length > 0) {
                 res.status(400)
                     .json({"message": "Command payload was invalid", "reason": JSON.stringify(result.errors)})
             } else {
-                command.transform(function (data) {
-                    kafka_producer.sendMessage("commands", data)
-                        .then(() => {
-                            res.status(200).json({"message": "success"});
+                command.transform((command_payload) => {
+                    console.log("Sending %s  to topic", JSON.stringify(command_payload));
+                    kafka_producer.sendMessage("commands", command_payload)
+                        .then((response) => {
+                            res.status(200).json({message: "command successfully persisted", "id": response.payload.id});
                         });
                 });
             }
